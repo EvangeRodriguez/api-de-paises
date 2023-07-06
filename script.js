e47ec25dee294bb9dd1e8d8d7ac8ec2c94c428f6
EvangeRodriguez <eva.irodriguez93@gmail.com>
2023-07-06 12:10:27 +0200

    nivel dos listo

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
-const countryList = document.getElementById('country-list'); //traemos el countryList del html 
-
 function getCountry() {
-  fetch('https://restcountries.com/v3.1/all') //nos conectamos con la API que vamos a utilizar
-    .then(response => response.json()) //convierte la respuesta de la API a JSON
-    .then(data => {
-      data.forEach(pais => { //recorremos el JSON
-        const list = document.createElement('li'); //crear un elemento HTML li
-        list.textContent = pais.name.common; //vinculamos el nombre del país con el elemento li
-
-        const flagImg = document.createElement('img'); //crear un elemento HTML img
-        flagImg.src = pais.flags.png; //vinculamos la bandera del país con la imagen
-
-        list.appendChild(flagImg); //agregar la imagen al li
-        countryList.appendChild(list); //agregar el li al ul
+    const countryList = document.getElementById('country-list')  //hacer referencia al HTML
+    const url = 'https://restcountries.com/v3.1/all'  //url de la API
+    //funcion para obtener los paises
+    fetch(url)//hacer la peticion
+      .then(response => response.json())//convertir la respuesta a json
+      .then(paises => {//recorrer el json
+        console.log(paises)//imprimir en consola
+        paises.forEach(pais => {//recorrer el json
+          const list = document.createElement('li') //crear un elemento li
+          list.textContent = pais.name.common;//agregar el nombre del pais
+          const flagImg = document.createElement('img')//crear un elemento img
+          flagImg.src = pais.flags.png//agregar la imagen del pais
+          list.appendChild(flagImg)//agregar la imagen al li
+          countryList.appendChild(list)//agregar el li al ul
+          list.addEventListener('mouseover', () => {//agregar un evento al li
+            getNeigCountries(pais.cca3)//llamar a la funcion para obtener los paises vecinos
+          })
+        })
+          //.catch(error => console.error(error))//imprimir el error en consola
       });
-    })
-    .catch(error => {
-      console.log('Error:', error);
-    });
-}
-
-getCountry(); //llamar a la función
+    }
+  getCountry();  //llamar a la funcion para obtener los paise
+  function getNeigCountries(countryCode) {//funcion para obtener los paises vecinos
+          const key = '0A3DLPAF3EHRMN3JSU6ZWTARNDBEBRGG'//key de la API
+          //const neighborUrl = `https://api.geodatasource.com/neighbouring-countries?key=${key}&country_code=${countryCode}`//url de la API
+        fetch(`https://api.geodatasource.com/neighbouring-countries?key=${key}&country_code=${countryCode}`)
+            .then(response => response.json())//convertir la respuesta a json
+            .then(infoPaises => {//recorrer el json
+              console.log(infoPaises)//imprimir en consola
+              const map = L.map('mapid');
+              const lat = infoPaises[0].latitude;
+              const lng = infoPaises[0].longitude;
+              map.setView([lat, lng], 13);
+              const neighboringCountriesList = document.createElement('div')//crear un elemento div
+              neighboringCountriesList.classList.add('neighboring-countries')//agregar la clase neighboring-countries
+              neighboringCountriesList.textContent = 'Neighboring Countries'//agregar el texto
+              const neighborCountries = document.createElement('ul')//crear un elemento ul
+              infoPaises.forEach(pais => {//recorrer el json
+                const neighborList = document.createElement('li')//crear un elemento li
+                neighborList.textContent = pais.country_name//agregar el nombre del pais vecino
+                neighborCountries.appendChild(neighborList)//agregar el li al ul
+              const selectedCountry = document.querySelector('.selected'); //seleccionar el pais
+                if (selectedCountry) {//si el pais esta seleccionado
+                  selectedCountry.classList.remove('selected');//remover la clase selected
+              }
+                countryList.classList.add('selected');//agregar la clase selected
+                countryList.appendChild(neighboringCountriesList);//agregar el ul al li
+              })
+            });
+        }
